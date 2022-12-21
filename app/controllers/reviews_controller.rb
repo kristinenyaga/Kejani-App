2d1b36d93cdcdebd13178a4b1abc91c8443f54e7
krisine <kristine.nyaga@student.moringaschool.com>
2022-12-21 11:44:25 +0300

    adds the crud methods for reviews

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,38 @@
+class ReviewsController < ApplicationController
+
+  def index
+    reviews=Review.all
+    render json: reviews
+  end
+
+  def show
+    review = Review.find_by(id:params[:id])
+    render json: review 
+  end
+
+  def destroy
+    review=Review.find_by(id:params[:id])
+    review.destroy
+    head :no_content
+
+  end
+
+  def create
+    review=Review.create!(review_params)
+    render json: review,status: :created
+  end
+
+
+  def update
+    review=Review.find_by(id:params[:id])
+    review.update!(review_params)
+    render json: review, status: :ok
+  end
+
+  private
+
+  def review_params
+
+    params.permit(:description,:unit_id,:user_id)
+  end
+end
