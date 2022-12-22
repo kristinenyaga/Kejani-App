c88f8cb52de8e70b0ac96a2bf4f6e41804ee389d
soila.torome <soila.torome@student.moringaschool.com>
2022-12-22 14:12:31 +0300

    add  routing for the login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Login/login";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route/>
+        <Route exact path="/login" element={<Login/>}/>
       </Routes>
     </Router>
   );
