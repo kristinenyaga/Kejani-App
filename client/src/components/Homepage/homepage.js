8dce0cdc89748ed7d10a3410edfecd95d6b9cb12
krisine <kristine.nyaga@student.moringaschool.com>
2022-12-23 02:02:41 +0300

    adds header section of the homepage

--- client/src/components/Homepage/homepage.js
+++ client/src/components/Homepage/homepage.js
@@ -3,15 +3,23 @@ import './homepage.css'
 export default function Homepage(){
   return(
     <>
-    <div>
-
     <NavBar />
-    <img className="homepage-img"
-    src='https://images.unsplash.com/photo-1459767129954-1b1c1f9b9ace?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80'>
-    </img>
+    <section>
+          <div className='black'>
+            <h1 className="header-title">Find Your Next <span>Perfect Place</span>With Us.</h1>
+            <h5 className="header-p">Kejani offers customers on demand experience for renting houses and providing transparency and almost seamless end-end service. </h5>
 
-    </div>
-    <h1>Home</h1>
+            <button className="header-button">Learn More</button>
+          </div>
+
+          <div className='red'>
+          <img className="header-img"
+            src="https://images.unsplash.com/photo-1600585154340-be6161a56a0c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80">
+            </img>
+           
+          </div>
+      
+    </section>
     </>
   )
 }
