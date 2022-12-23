a35b90f6471d48b82b3027c6ab1a55d386ec75de
krisine <kristine.nyaga@student.moringaschool.com>
2022-12-23 10:54:07 +0300

    adds the export function in login component

--- client/src/components/Login/login.js
+++ client/src/components/Login/login.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './login.css';
-function Login({ onLogin }) {
+ export default function Login({ onLogin }) {
     const [errors, setErrors] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
@@ -65,10 +65,3 @@ function Login({ onLogin }) {
     )
 };
 
-export default function Login(){
-  return(
-    <>
-    <h1>Login</h1>
-    </>
-  )
-}
