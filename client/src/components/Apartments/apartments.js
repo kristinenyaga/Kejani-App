a23766781ca85e7f4724e152dfa2376091bf8558
Simion <simion.okola@student.moringaschool.com>
2022-12-27 13:30:13 +0300

    style(apartments) styling fonts

--- client/src/components/Apartments/apartments.js
+++ client/src/components/Apartments/apartments.js
@@ -1,20 +1,15 @@
-import React, { useState} from 'react'
+import React from 'react'
 import './apartments.css'
 
 function Apartments( {propertyDetail} ) {
 
+  console.log(propertyDetail)
 
   function formatAmount(price) {
     const amountFormatter = new Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD'});
     return amountFormatter.format(price);
 
 }
-
-const [moreDetails, setMoreDetails] = useState()
-
-    console.log(propertyDetail)
-
-    
 
   return (
     <>
@@ -24,12 +19,14 @@ const [moreDetails, setMoreDetails] = useState()
           <img className= 'properties'src={propertyDetail.image_url} />
         </div>
         <div>
-          <h4 className='det'>Property Name: {propertyDetail.property_name}</h4>
-          <h4 className='det'>Location: {propertyDetail.location}</h4>
-          <h4 className='det'>Rent: { formatAmount (propertyDetail.price/100)} </h4>
+          <p4 className='det'>Property Name: {propertyDetail.property_name}</p4>
+          <p4 className='det'>Location: {propertyDetail.location}</p4>
+          <p4 className='det'>Rent: { formatAmount (propertyDetail.price/100)} </p4>
           <button className='but' type='submit'>More details</button>
         </div>
       </div>
+
+      
      
 
     </>
