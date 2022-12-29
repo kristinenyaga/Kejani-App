2d224de5f93e1cdb6713efc060bb4eabe0f4f4e0
soila.torome <soila.torome@student.moringaschool.com>
2022-12-29 11:34:26 +0300

    remove unnecessary routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,18 @@
-import React, {useEffect, useState} from 'react'
+// import React, {useEffect, useState} from 'react'
 import './App.css';
 import Apartments from './components/ApartmentCard/apartmentCard';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import { Routes, Route } from 'react-router-dom'
 import SignUp from './components/SignUp/signup.js'
-import { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { useState } from "react";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from './components/Login/login';
 import Homepage from './components/Homepage/homepage';
 import ApartmentCard from './components/ApartmentCard/apartmentCard';
 import ImageCard from './components/ImageCard/imageCard';
 
 function App() {
-  const [user, setUser] = useState(null);
+  // const [user, setUser] = useState(null);
   return (
     <> 
       <Routes>
@@ -22,7 +22,7 @@ function App() {
             {/* <Routes> */}
               <Route exact path="/signup" element={<SignUp />} />
               <Route exact path="/login" element={<Login />} />
-              <Route exact path="/" element={ <Homepage user={user} setUser={setUser}/>} />
+              <Route exact path="/" element={ <Homepage/> }/>
               <Route exact path="/" element={ <Homepage />} />
               <Route exact path="/imagecard" element={ <ImageCard />} />
               
