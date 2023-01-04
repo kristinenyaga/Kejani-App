be911673d59d8b558e7df06642b4a1634d784afb
BenjaminOtieno <benjamin.otieno@moringaschool.com>
2023-01-04 13:25:11 +0300

    ignore commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { createContext, useState } from 'react'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
+//import 'antd/dist/reset.css';
 import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom'
 import SignUp from './components/SignUp/signup.js'
 import Login from './components/Login/login';
@@ -10,10 +11,13 @@ import Addapartment from './components/Dashboard/Addapartment/addapartment'
 import Review from './components/Dashboard/Reviews/review'
 import Tenants from './components/Dashboard/tenants/tenants'
 
-import style from './App.css';
-//import Layout from './components/Dashboard/layout/layout';
+import './App.css';
+import Layout from './components/Dashboard/layout/layout';
+import TopSection from './components/Dashboard/topbar/topbar';
 
-function App() {
+export const AppContext = createContext(null);
+
+export default function App() {
   const [user, setUser] = useState("")
   const [role, setRole] = useState("")
   const navigate = useNavigate("")
@@ -25,7 +29,7 @@ function App() {
     if (role === 'user') {
       navigate('/')
     } else if (role === 'lister') {
-      navigate('/defaultinbox')
+      navigate('/layout')
 
 
 
@@ -37,20 +41,29 @@ function App() {
   const [latestPost, setLatestPost] = useState(AppContext)
 
   return (
-    <>
-      {
-        role === 'user' ?
-          (<Routes>
-            <Route exact path="/signup" element={<SignUp />} />
-            <Route exact path="/login" element={<Login onLogin={onLogin} setRole={setRole} />} />
-            <Route exact path="/" element={<Homepage />} />
-          </Routes>
-          ) :
-          (
-            <Layout />
-          )
-      }
-    </>
+    <AppContext.Provider value={{ latestPost, setLatestPost }}>
+
+      <Routes>
+        <Route exact path="/signup" element={<SignUp />} />
+        <Route exact path="/login" element={<Login onLogin={onLogin} setRole={setRole} />} />
+        <Route exact path="/" element={<Homepage />} />
+        <Route exact path="/layout/*" element={<Layout />} />
+        <Route exact path="/inbox" element={<DefaultInbox />} />
+        <Route exact path="/reviews" element={<Review />} />
+        <Route exact path="/addapartment" element={<Addapartment user={user} />} />
+        <Route exact path="/tenants" element={<Tenants />} />
+        <Route path="/topbar" element={<TopSection />} />
+
+
+
+
+
+
+
+      </Routes>
+
+
+    </AppContext.Provider>
 
 
 
