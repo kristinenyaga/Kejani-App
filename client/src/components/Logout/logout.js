be5a81a7750eeea24b087edd880a87cbdbd4b03b
krisine <kristine.nyaga@student.moringaschool.com>
2023-01-05 14:27:52 +0300

    this commit makes the code DRY

--- client/src/components/Logout/logout.js
+++ client/src/components/Logout/logout.js
@@ -1,38 +1,25 @@
 import React, {useEffect, useState} from 'react'
-import {Link} from 'react-router-dom'
+import {Link, Navigate, useNavigate} from 'react-router-dom'
 import Nav from 'react-bootstrap/Nav';
 
 function Logout({user, setUser}){
-
-    // const [user, setUser] = useState(null)
-
-    // useEffect(() => {
-    //     fetch("/me").then((r) => {
-    //       if (r.ok) {
-    //         r.json().then((user) => setUser(user));
-    //       }
-    //     });
-    //   }, []);
+const navigate = useNavigate()
+   
 
     function handleLogoutClick() {
         fetch("/logout", { method: "DELETE" }).then((r) => {
           if (r.ok) {
             setUser(null);
+            navigate('/')
           }
         });
       }    
 
     return(
         <>
-        {user ? (
+        
           <button onClick={handleLogoutClick} >Logout</button>
-        ) : (
-          <div >
-            <Nav.Link as={Link} className='auth' to="/login">Login</Nav.Link>
-            <Nav.Link as={Link} className='auth' to="/signup">Signup</Nav.Link>
-            
-          </div>
-        )}
+        
         </>
     )
 }
