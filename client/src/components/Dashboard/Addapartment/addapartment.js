1c45596ba3c44c22557a664acfc84d159105042c
krisine <kristine.nyaga@student.moringaschool.com>
2023-01-05 10:55:16 +0300

    this commit adds the apartments section

--- client/src/components/Dashboard/Addapartment/addapartment.js
+++ client/src/components/Dashboard/Addapartment/addapartment.js
@@ -1,8 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {  useNavigate } from 'react-router-dom'
+import Apartment from './apartment'
 
-export default function AddApartment() {
+export default function AddApartment({user,setApartment,apartment,handleApartment}) {
+  console.log(user)
+  
+  
+  const navigate =useNavigate("")
+  function handleClick(){
+    navigate('/apartmentform')
+  }
+
+  function handleUnitClick(){
+    navigate('/unitform')
+  }
   return (
-    <div>addapartmAdd</div>
+    <div className="section2">
+    <h1 className="section2h1">
+    <i class="fa-solid fa-house-circle-check"></i>Kejani App
+    </h1>
+
+    <div className='addapartment'>
+      <h3 className='apartment-h3'>Your Apartments</h3>
+
+    </div>
+    <h3 className='apartment-add' onClick={handleClick}>Add apartment <i class="fa-solid fa-arrow-right-from-bracket"></i></h3>
+
+
+    <div className="section2container">
+      <div className="section2row">
+        
+      {user.apartments?.map(apartment =>(
+         <Apartment  apartment={apartment} handleApartment={handleApartment} setApartment={setApartment} />
+     
+      ))}
+      {/* {posts?.map(post => <Card details={post}/>)} */}
+       
+      </div>
+
+    </div>
+    </div>
   )
 }
 
