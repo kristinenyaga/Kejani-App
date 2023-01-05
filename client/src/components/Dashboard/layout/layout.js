8d547103fbebbbb0439d89a68b8abcfa5a8dc602
krisine <kristine.nyaga@student.moringaschool.com>
2023-01-05 10:57:02 +0300

    this commit changes tha layout for the dashboard

--- client/src/components/Dashboard/layout/layout.js
+++ client/src/components/Dashboard/layout/layout.js
@@ -9,19 +9,31 @@ import Review from "../Reviews/review";
 import Inbox from "../defaultinbox/defaultinbox";
 import Tenants from "../tenants/tenants";
 import AddApartment from "../Addapartment/addapartment";
+import SideBarPages from "../Routes/sideBarPages";
 
-const Layout = () => {
+const Layout = ({user,setApartment, apartment}) => {
+  function handleApartment(apartment){
+    setApartment(apartment)
+  }
   return (
-    <SideBar>
-    <Routes>
-      <Route path="/topbar" element={<TopSection />} />
-      {/* <Route path="/dashboard" element={<Dashboard />} /> */}
-      <Route path="/addapartment" element={<AddApartment/>} />
-      <Route path="/reviews" element={<Review />} />
-      <Route path="/inbox" element={<Inbox />} />
-      <Route path="/tenants" element={<Tenants />} />
-    </Routes>
-  </SideBar>
+    <React.Fragment>
+    <section>
+      <div>
+        <div>
+          <TopSection />
+        </div>
+
+        <div className="w-full h-screen flex ">
+          <div>
+            <SideBar />
+          </div>
+          <div className="w-full h-screen">
+            <SideBarPages user={user} setApartment={setApartment} handleApartment={handleApartment} apartment={apartment}/>
+          </div>
+        </div>
+      </div>
+    </section>
+  </React.Fragment>
   )
 };
 
