3c6d7957b4f4c757cf6614e4329a5dd0fdec7365
krisine <kristine.nyaga@student.moringaschool.com>
2023-01-09 15:17:09 +0300

    modifies css styling for the apartmentcard

--- client/src/components/ApartmentCard/apartmentCard.js
+++ client/src/components/ApartmentCard/apartmentCard.js
@@ -1,19 +1,21 @@
 import React from 'react'
 import Apartments from '../Apartments/apartments';
-
+import "./apartmentCard.css"
 function ApartmentCard({detail}){
 
     console.log(detail)
 
     return (
-
-        <>
-      <h1 className='section2h1'>Apartments</h1>
-
+      <>
+      <h1 className='section2h1'>Our Apartments</h1>
+      <div className='aptdisplayflex'>
         {detail.map((propertyDetail) => (
             <Apartments key={propertyDetail.id} propertyDetail={propertyDetail} />
         ))}
-        </>
+        
+       </div>
+      </>
+        
     )
 }
 
