ea4714e1f32b7eb5ad00cc80d4d2f783d0df25ef
krisine <kristine.nyaga@student.moringaschool.com>
2023-01-16 08:15:40 +0300

    modifications of filter section in userpage

---
+++ client/src/components/userspage/empty.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Empty } from 'antd';
+
+const Emptyy= () => {
+  return (
+    <div className='mt-6 content-center'>
+      <Empty/>
+      <h3 className='text-xl text-center'>No results found</h3>
+    </div>
+  )
+}
+
+export default Emptyy
--- client/src/components/userspage/userpage.js
+++ client/src/components/userspage/userpage.js
@@ -3,21 +3,18 @@ import UnitInfor from '../Units/unitInfor'
 import UnitSearch from '../Units/unitSearch'
 import Unitspecs from '../Units/unitspecs'
 import Category from './category'
+import Emptyy from './empty'
 import PriceFilter from './pricefilter'
+import UserNav from './usernavbar'
 import './userpage.css'
 function UsersPage({handleRequest}) {
   const [units,setUnits]=useState([])
+  const [newunits,setNewUnits]=useState(units)
   const [selectedCategory, setSelectedCategory] = useState("");
   const [search, setSearch] = useState([])
   const [selectedPrice, setSelectedPrice] = useState([5000, 50000]);
 
-  const [category, setCategory] = useState([
-    { id: 1, checked: false, label: 'bedsitter' },
-    { id: 2, checked: false, label: '1bedroom' },
-    { id: 3, checked: false, label: '2bedroom' },
-    { id: 4, checked: false, label: '3bedroom' }
-
-  ]);
+  
   const categories = units
   .map((unit) => unit.category)
   .filter(
@@ -29,112 +26,45 @@ function UsersPage({handleRequest}) {
     setSelectedPrice(value);
   };
 
-  const handleChangeChecked = (cat,id) => {
-    console.log(cat)
-    const categoryStateList = category;
-    const changeCheckedCategories = categoryStateList.map((item) =>
-      item.id === id ? { ...item, checked: !(item.checked )} : item
-    );
-    setCategory(changeCheckedCategories);
-    console.log(category)
-  };
+   console.log(units)
+  
   useEffect(()=>{
     fetch("/units")
     .then((res)=>res.json())
     .then((data)=>{
       setUnits(data)
+      setNewUnits(data)
     })
   }, [])
-  const filterItem = (categItem) => {
-    const updatedItems = units.filter((curElem) => {
-      if(categItem === "All"){
-        setUnits(units)
 
-    }
-    else{
-      return curElem.category === categItem;
-    }
-        
-    });
 
-    setUnits(updatedItems);
-}
-
-  function applyFilters(){
-    let displayedUnits=units
-     
-        if(selectedCategory){
-            displayedUnits = displayedUnits.filter(
-                (unit) =>{
-                    if(selectedCategory === "All"){
-                        return unit.category !== selectedCategory
-
-                    }
-                    else{
-                        return unit.category === selectedCategory
-                    }
-                }
-                       
-              );
-        }
-        // Cuisine Filter
-          const categoryChecked = category
-          .filter((item) => item.checked)
-          .map((item) => item.label.toLowerCase());
-
-        if (categoryChecked.length) {
-          displayedUnits = displayedUnits.filter((item) =>
-          // categoryChecked.includes(item.category)
-          categoryChecked.includes(item.category)
-          );
-        }
-       // Search Filter
-    if (search) {
-      displayedUnits = displayedUnits.filter(
-        (item) =>
-          item.apartment.apartment_type.toLowerCase().search(search.toLowerCase().trim()) !==
-          -1
-      );
-    }
-        
-        const minPrice = selectedPrice[0];
-        const maxPrice = selectedPrice[1];
-
-        displayedUnits = displayedUnits.filter(
-        (item) => item.price >= minPrice && item.price <= maxPrice
-        );
-        setUnits(displayedUnits)
-
-    }
-        useEffect(() => {
-            applyFilters();
-          }, [selectedCategory,search,selectedPrice]);
-        
-        
-        
+ 
 
 
   
   return (
     <>
-    <UnitSearch value={search}
-        changeInput={(e) => setSearch(e.target.value)} />
+    <UserNav/>
+       <UnitSearch 
+       setSearch = {setSearch}
+       units={units}
+      setUnits={setUnits}
+      newunits={newunits}
+      search={search}/>
     <div className="userspage">
       
       <Category
-        categories={["All", ...categories]}
-        selectedCategory={selectedCategory}
-        setSelectedCategory={setSelectedCategory}
-        changePrice={handleChangePrice}
-        value={selectedPrice}
-        category={category}
-        changeChecked={handleChangeChecked}
-        filterItem={filterItem}
-        units={units}
-        setUnits={setUnits}
+      units={units}
+      setUnits={setUnits}
+      newunits={newunits}
+      selectedPrice={selectedPrice}
+      setSelectedPrice={setSelectedPrice}
+     
         /> 
         <div className='units'>
-        {units.map((data) => (
+
+        {units.length === 0?<Emptyy/>:
+        units.map((data) => (
             <Unitspecs key={data.id} data={data} handleRequest={handleRequest} />
         ))}
         </div>
