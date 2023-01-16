45486c4f2db9dc6cd1f04f14799d0f5812cd51c2
krisine <kristine.nyaga@student.moringaschool.com>
2023-01-16 08:17:21 +0300

    adds the requestunit relationship

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,6 +2,7 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :username, :location, :email,:phone_number,:role
   has_many :units
   has_many :apartments
+  has_many :requestunits
   # has_many :reviews
  
 end
