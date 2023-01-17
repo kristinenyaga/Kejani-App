05ed2a538327479cdc294c07dd00513c6f599ee6
Simion <simion.okola@student.moringaschool.com>
2023-01-18 02:04:18 +0300

    refactor(tenant) merge code

--- client/src/components/Dashboard/tenants/tenants.js
+++ client/src/components/Dashboard/tenants/tenants.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+import Tenant from './tenant';
 
-const Tenants = () => {
+function Tenants() {
+  const [users, setUsers] = useState([])
+
+  useEffect(() =>{
+    fetch('/users')
+    .then((r) => r.json())
+    .then((data) => setUsers(data))
+  }, []);
+
   return (
-    <div>
-      <h1>Tenants page</h1>
+    // <div>
+    //   <h3>Tenants</h3>
+    // </div>
+    <div className='overflow-auto rounded-lg shadow hidden md:block'>
+      <table className='w-full'>
+        <thead className='bg-gray-50 border-b-2 border-gray-200'>
+          <tr>
+            <th className='p-3 text-sm font-semibold tracking-wide text-left'> Name</th>
+            <th scope='p-3 text-sm font-semibold tracking-wide text-left'> Contact</th>
+            <th scope='p-3 text-sm font-semibold tracking-wide text-left'> Email</th>
+            <th scope='p-3 text-sm font-semibold tracking-wide text-left'> Location</th>
+          </tr>
+        </thead>
+        {users.map((user) =>
+        <Tenant key={user.id} user={user}/>
+        
+        )}
+      </table>
     </div>
   );
 };
